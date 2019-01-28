51c01ed09d0e521fe4ef3b98600165947e1b0fb5
NNrepos <nivimano@gmail.com>
2019-01-28 17:03:14 +0200

    chapter 6 completed.

--- rango_app/models.py
+++ rango_app/models.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Catergories"
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    date = models.DateField()
+
+    def __unicode__(self):
+        return self.title
